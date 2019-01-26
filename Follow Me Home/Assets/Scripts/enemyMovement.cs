2cc4738a004ed21cadcbc44bde4a82d3757c7fb4
Mike <mdsiegfried@fullsail.edu>
2019-01-26 12:49:35 -0500

    Added Navmesh and NPC navigation.

--- Follow Me Home/Assets/Scripts/enemyMovement.cs
+++ Follow Me Home/Assets/Scripts/enemyMovement.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class enemyMovement : MonoBehaviour
 {
@@ -8,33 +9,31 @@ public class enemyMovement : MonoBehaviour
     public Transform[] waypoints;
     public float speed;
 
-    private bool toNextWaypoint = true;
-
+    private NavMeshAgent agent;
     private int currentWaypoint = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
+        agent.SetDestination(waypoints[currentWaypoint].position);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.right * Time.deltaTime * speed);
-
-        if (currentWaypoint < waypoints.Length)
-        {
-            moveToNextWaypoint();
-        }
+        //transform.Translate(Vector3.right * Time.deltaTime * speed);
+        moveToNextWaypoint();
     }
 
     private void moveToNextWaypoint()
     {
-        if (toNextWaypoint)
+        float distance = Vector3.Distance(waypoints[currentWaypoint].position, transform.position);
+        Vector3 nextWaypoint = waypoints[currentWaypoint + 1].position;
+
+        if (distance < waypoints.Length)
         {
-            Vector3 nextWaypoint = waypoints[currentWaypoint].position;
-            Vector3 direction = nextWaypoint - transform.position;
+
         }
     }
 }
